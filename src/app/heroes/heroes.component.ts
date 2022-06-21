d7ff029518a741024bcd2de9bb2f638a5f9b18eb
mirkomaga <56160646+mirkomaga@users.noreply.github.com>
2022-06-21 20:18:01 +0200

    Oke

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.less']
+})
+
+export class HeroesComponent implements OnInit {
+  
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService,
+    private messageService: MessageService) { }
+  
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+      this.selectedHero = hero;
+      this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+}
